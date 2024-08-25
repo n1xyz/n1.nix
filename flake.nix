44266bea40c9f744359ee5afb10d2acb910c83c2
Sheheryar Parvaz <me@cherryman.org>
2024-08-25 19:37:53 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Various solc versions";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          solc = pkgs.callPackage ./solc-0.8.26.nix { };
+        in
+        {
+          packages.default = solc;
+        };
+    };
+}
