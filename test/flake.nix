dcbfac160a4e3cea0fd97fa9735cb89837ec4914
Sheheryar Parvaz <me@cherryman.org>
2024-08-26 10:04:59 -0400

    Package solc v0.8.26

--- test/flake.nix
+++ test/flake.nix
@@ -1,16 +1,14 @@
 {
-  description = "Various solc versions";
+  description = "Test flake";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    layern.url = "path:../.";
   };
 
   outputs =
-    inputs@{ flake-parts, nixpkgs, ... }:
-    let
-      packagesFor = pkgs: { solc-0_8_26 = pkgs.callPackage ./solc-0.8.26.nix { }; };
-    in
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
@@ -28,10 +26,12 @@
           ...
         }:
         {
-          packages = packagesFor pkgs;
+          devShells.default = pkgs.mkShell { buildInputs = with pkgs; [ solc-0_8_26 ]; };
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ inputs.layern.overlays.default ];
+            config = { };
+          };
         };
-      flake = {
-        overlays.default = final: prev: packagesFor prev;
-      };
     };
 }
