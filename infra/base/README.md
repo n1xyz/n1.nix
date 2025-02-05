f14cb4bcdaa49fa3719bc0d3c14ae948b01fa371
dzmitry-lahoda <dzmitry@lahoda.pro>
2025-02-05 22:27:57 +0000

    asserting bucket

--- infra/base/README.md
+++ infra/base/README.md
@@ -1,1 +1,3 @@
-Script to setup base NixOs image into Latitudesh via iPXE stored at Backblaze
+Script to setup base NixOs image into Latitudesh via iPXE stored at Backblaze.
+
+Bucket MUST be public without locks and without encryption.
--- infra/base/base.tf
+++ infra/base/base.tf
@@ -11,17 +11,42 @@ provider "b2" {
 
 }
 
-resource "b2_bucket" "base-image" {
-  bucket_name = "base-image"
-  bucket_type = "allPublic"
+data "b2_bucket" "n1-nix-public" {
+  bucket_name = "n1-nix-public"
+  # bucket_type = "allPublic"
 }
 
-resource "b2_bucket_file_version" "ipxe" {
-    bucket_id = b2_bucket.base-image.id
-    file_name = "base.ipxe"
-    source = file("${path.module}/../../result/netboot.ipxe")
+resource "b2_bucket_file_version" "ipxe_1" {
+    bucket_id = data.b2_bucket.n1-nix-public.id
+    file_name = "ipxe_1.ipxe"
+    content_type = "text/plain"
+    source = "${path.module}/../../result/netboot.ipxe"
+}
+
+
+resource "b2_bucket_file_version" "initrd_1" {
+    bucket_id = data.b2_bucket.n1-nix-public.id
+    file_name = "initrd_1"
+    content_type = "application/octet-stream"
+    source = "${path.module}/../../result/initrd"
+}
+
+
+resource "b2_bucket_file_version" "kernel_1" {
+    bucket_id = data.b2_bucket.n1-nix-public.id
+    file_name = "kernel_1"
+    content_type = "application/octet-stream"
+    source = "${path.module}/../../result/bzImage"
 }
 
 output "ipxe_url" {
-    value = b2_bucket_file_version.ipxe.id
+    value = b2_bucket_file_version.ipxe_1.file_id
+}
+
+output "initrd_url" {
+    value = b2_bucket_file_version.initrd_1.file_id
+}
+
+output "kernel_url" {
+    value = b2_bucket_file_version.kernel_1.file_id
 }
