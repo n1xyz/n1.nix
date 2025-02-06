5ea3064703094b9973f64e6af0cd8cc77e0c950e
dzmitry-lahoda <dzmitry@lahoda.pro>
2025-02-06 22:55:28 +0000

    test

--- infra/base/base.tf
+++ infra/base/base.tf
@@ -4,6 +4,10 @@ terraform {
       source = "Backblaze/b2"
       version = "0.10.0"
     }
+    latitudesh = {
+      source  = "latitudesh/latitudesh"
+      version = "1.2.0"
+    }    
   }
 }
 
@@ -11,42 +15,85 @@ provider "b2" {
 
 }
 
+variable "LATITUDESH_AUTH_TOKEN" {
+  type = string
+  sensitive = true
+  nullable = false  
+}
+
+variable "LATITUDESH_PROJECT" {
+  nullable = false
+  type = string
+}
+
+variable "SSH_PUBLIC_KEY" {
+  nullable = false
+  type = string
+}
+
+provider "latitudesh" {
+  auth_token = var.LATITUDESH_AUTH_TOKEN
+}
+
 data "b2_bucket" "n1-nix-public" {
   bucket_name = "n1-nix-public"
-  # bucket_type = "allPublic"
+  # bucket_type = "allPublic" # must be to share with Latitude
 }
 
-resource "b2_bucket_file_version" "ipxe_1" {
-    bucket_id = data.b2_bucket.n1-nix-public.id
-    file_name = "ipxe_1.ipxe"
-    content_type = "text/plain"
-    source = "${path.module}/../../result/netboot.ipxe"
+# resource "b2_bucket_file_version" "ipxe_003" {
+#     bucket_id = data.b2_bucket.n1-nix-public.id
+#     file_name = "ipxe_003.ipxe"
+#     content_type = "text/plain"
+#     source = "${path.module}/../../result/netboot.ipxe"
+#     # depends_on = [ b2_bucket_file_version.initrd_003 ]
+# }
+
+
+# resource "b2_bucket_file_version" "initrd_003" {
+#     bucket_id = data.b2_bucket.n1-nix-public.id
+#     file_name = "initrd_003"
+#     content_type = "application/octet-stream"
+#     source = "${path.module}/../../result/initrd"
+#     depends_on = [ b2_bucket_file_version.kernel_003 ]
+# }
+
+
+# resource "b2_bucket_file_version" "kernel_003" {
+#     bucket_id = data.b2_bucket.n1-nix-public.id
+#     file_name = "kernel_003"
+#     content_type = "application/octet-stream"
+#     source = "${path.module}/../../result/bzImage"
+# }
+
+# # what we get using:
+# # backblaze-b2 file url b2://{bucket_name}/{file_name}
+# output "ipxe_url" {
+#     value = "https://f003.backblazeb2.com/file/${data.b2_bucket.n1-nix-public.bucket_name}/${b2_bucket_file_version.ipxe_003.file_name}"
+# }
+
+# output "initrd_url" {
+#     value =  "https://f003.backblazeb2.com/file/${data.b2_bucket.n1-nix-public.bucket_name}/${b2_bucket_file_version.initrd_003.file_name}"
+# }
+
+# output "kernel_url" {
+#     value = "https://f003.backblazeb2.com/file/${data.b2_bucket.n1-nix-public.bucket_name}/${b2_bucket_file_version.kernel_003.file_name}"
+# }
+
+resource "latitudesh_ssh_key" "latitudesh_ssh_key" {
+  project    = var.LATITUDESH_PROJECT
+  name       = "init_key"
+  public_key = var.SSH_PUBLIC_KEY
 }
 
-
-resource "b2_bucket_file_version" "initrd_1" {
-    bucket_id = data.b2_bucket.n1-nix-public.id
-    file_name = "initrd_1"
-    content_type = "application/octet-stream"
-    source = "${path.module}/../../result/initrd"
+resource "latitudesh_server" "server" {
+  hostname         = "c2-small-x86-nixos-22-11-sao-nord"
+  # "https://f003.backblazeb2.com/file/${data.b2_bucket.n1-nix-public.bucket_name}/${b2_bucket_file_version.ipxe_003.file_name}"
+  ipxe_url = "https://raw.githubusercontent.com/latitudesh/examples/refs/heads/main/custom-images/ubuntu-24/boot.ipxe" 
+  operating_system = "ipxe"
+  plan             = "c2-small-x86"
+  project          = var.LATITUDESH_PROJECT   # MUST be ID, not name, name leads to recreation
+  site             = "SAO"  
+  ssh_keys         = [latitudesh_ssh_key.latitudesh_ssh_key.id]
+  tags = [] # must use, beause auto is [], not null
 }
 
-
-resource "b2_bucket_file_version" "kernel_1" {
-    bucket_id = data.b2_bucket.n1-nix-public.id
-    file_name = "kernel_1"
-    content_type = "application/octet-stream"
-    source = "${path.module}/../../result/bzImage"
-}
-
-output "ipxe_url" {
-    value = b2_bucket_file_version.ipxe_1.file_id
-}
-
-output "initrd_url" {
-    value = b2_bucket_file_version.initrd_1.file_id
-}
-
-output "kernel_url" {
-    value = b2_bucket_file_version.kernel_1.file_id
-}
