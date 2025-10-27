cd25cabe975f80ff91ac956a5bcc11fc757519ef
dzmitry-lahoda <dzmitry@lahoda.pro>
2025-10-27 16:41:21 +0000

    more reusable musl build params

--- packages/cargo-build-static-release.nix
+++ packages/cargo-build-static-release.nix
@@ -2,13 +2,17 @@
 # `$@` all user provided parameters to cargo zigbuild
 {
   cargo-zigbuild,
-  # must have `x86_64-unknown-linux-musl` target
   rustc,
   git,
   writeShellApplication,
   # for example, to use `protoc` or `mimalloc` (it is runtime input because is needed when script runs to build)
   extra-runtimeInputs ? [ ],
+  # flake or other attrset with `rev` attribute which is git commit hash string
   name ? "cargo-build-static-release",
+  # must have `target`, defaults to `x86_64-unknown-linux-musl`
+  target ? "x86_64-unknown-linux-musl",
+  # flake for hermetic build
+  self ? null,
 }:
 
 writeShellApplication {
@@ -19,21 +23,32 @@ writeShellApplication {
     git
   ] ++ extra-runtimeInputs;
   text = ''
-    if [ -n "$(git status --porcelain)" ]; then
-      echo >&2 'dirty tree, refusing to build; stash or commit your changes'
-      exit 1
-    fi
+    ${
+      if self == null then
+        ''
+          if [ -n "''$(git status --porcelain)" ]; then
+            echo >&2 'dirty tree, refusing to build; stash or commit your changes'
+            exit 1
+          fi
+        ''
+      else
+        ''
+          echo "Git Commit: ${self.rev}"
+        ''
+    }
+
     set -x
     # zig 0.14.0 causes ProcessFdQuotaExceeded, so raise the limit.
     # this fails if some parent process has already called `ulimit -n`, so
     # you may need to open a new shell in that case.
-    ulimit -n 8192
+    ${if self == null then "ulimit -n 8192" else ""}
+
     # `zig cc` sets `-Werror=date-time` which breaks some dependencies. not needed in this case
     # as this tool is not meant for reproducibility
     export CFLAGS="-Wno-error=date-time"
     export RUSTFLAGS="-C target-feature=+crt-static"
     cargo zigbuild \
-        --target=x86_64-unknown-linux-musl \
+        --target=${target} \
         --release \
         "$@"
   '';
