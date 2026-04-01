73af35c6dfe5f1276315f6ffb9bac0960ee1b0e9
Javier A. Pollak <javi.po.123@gmail.com>
2026-04-01 10:25:12 -0400

    add axiom-cli drv

--- flake.nix
+++ flake.nix
@@ -4,23 +4,32 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs =
-    inputs@{ flake-parts, nixpkgs, ... }:
+    inputs@{ flake-parts, ... }:
     let
-      packagesFor = pkgs: rec {
-        solc-0_8_26 = pkgs.callPackage ./solc-0.8.26.nix { };
-        spl-token = pkgs.callPackage ./spl-token.nix { };
-        agave-platform-tools = pkgs.callPackage ./agave-platform-tools.nix { };
-        agave-cli = pkgs.callPackage ./agave-cli.nix {
-          inherit agave-platform-tools;
+      packagesFor =
+        {
+          pkgs,
+          pkgsUnstable,
+        }:
+        rec {
+          solc-0_8_26 = pkgs.callPackage ./solc-0.8.26.nix { };
+          spl-token = pkgs.callPackage ./spl-token.nix { };
+          agave-platform-tools = pkgs.callPackage ./agave-platform-tools.nix { };
+          agave-cli = pkgs.callPackage ./agave-cli.nix {
+            inherit agave-platform-tools;
+          };
+          axiom-cli = pkgs.callPackage ./axiom-cli.nix {
+            go = pkgsUnstable.go_1_26;
+          };
+          shank = pkgs.callPackage ./shank.nix { };
+          squads-cli = pkgs.callPackage ./squads-cli.nix { };
+          cargo-build-static-release = pkgs.callPackage ./packages/cargo-build-static-release.nix { };
+          bun-enforce = pkgs.callPackage ./packages/bun-enforce.nix { };
         };
-        shank = pkgs.callPackage ./shank.nix { };
-        squads-cli = pkgs.callPackage ./squads-cli.nix { };
-        cargo-build-static-release = pkgs.callPackage ./packages/cargo-build-static-release.nix { };
-        bun-enforce = pkgs.callPackage ./packages/bun-enforce.nix { };
-      };
     in
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
@@ -39,7 +48,10 @@
           ...
         }:
         {
-          packages = packagesFor pkgs;
+          packages = packagesFor {
+            inherit pkgs;
+            pkgsUnstable = inputs.nixpkgs-unstable.legacyPackages.${system};
+          };
           formatter = pkgs.nixfmt-rfc-style;
           devShells.default = pkgs.mkShell {
             # yarn-berry-fetcher used to compute yarn missing-hashes.json
@@ -62,7 +74,12 @@
               '';
         };
       flake = {
-        overlays.default = final: prev: packagesFor prev;
+        overlays.default =
+          final: prev:
+          packagesFor {
+            pkgs = prev;
+            pkgsUnstable = inputs.nixpkgs-unstable.legacyPackages.${prev.stdenv.buildPlatform.system};
+          };
 
         nixConfig = {
           extra-substituters = [ "https://n1.cachix.org" ];
